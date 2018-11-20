9e8ca836afba6b1dfce6920b2eed43085d4aeaeb
gretavaresano <greta.varesano@gmail.com>
2018-11-21 00:04:34 +0100

    full code

--- sketch.js
+++ sketch.js
@@ -1,11 +1,26 @@
-function preload(){
-  // put preload code here
+let angle = 0;
+
+
+function preload() {
+  hand = loadModel('hand.obj');
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight, WEBGL);
+    setShakeThreshold(2);
+
 }
 
 function draw() {
-  // put drawing code here
+background(0);
+
+var aX = rotationX/2;
+var aY = rotationY/2;
+  directionalLight(240, 240, 240, aX, aY, -1);
+  ambientMaterial(250);
+  scale(50,50,50);
+  rotateY(angle * 0.05);
+  translate(0, 0, 0);
+  model(hand);
+  angle += 0.03;
 }
